346f396e284931fa867041a5785eb01cf73299fd
tomas <Homero_77>
2022-04-13 19:52:55 -0300

    boton barra superior para filtrar

--- app/src/main/java/com/example/autos/MainActivity.kt
+++ app/src/main/java/com/example/autos/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.autos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Hace funcionar el back button del ActionBar
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.listaFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/autos/fragments/ListaFragment.kt
+++ app/src/main/java/com/example/autos/fragments/ListaFragment.kt
@@ -4,21 +4,22 @@ import android.content.ContentValues
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.autos.R
-import com.example.autos.models.Publicacion
 import com.example.autos.adapters.PublicacionesAdapter
+import com.example.autos.models.Publicacion
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.toObject
 import java.util.*
 
@@ -28,21 +29,18 @@ class ListaFragment : Fragment() {
     private lateinit var publicacionesRecyclerView: RecyclerView
     var publicaciones = ArrayList<Publicacion>()
     private lateinit var fragmentView: View
-    private lateinit var btn_filtros : Button
     var postsList = ArrayList<Publicacion>()
     private lateinit var lista : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreate(savedInstanceState)
-        fragmentView = inflater.inflate(R.layout.fragment_lista,container,false)
+        fragmentView = inflater.inflate(R.layout.fragment_lista, container, false)
         publicacionesRecyclerView = fragmentView.findViewById(R.id.recycler_publicaciones)
-        btn_filtros = fragmentView.findViewById(R.id.button)
         getPostFromFirebase()
 
         configureRecyclerView()
@@ -52,7 +50,7 @@ class ListaFragment : Fragment() {
     private fun configureRecyclerView(){
         publicacionesRecyclerView.setHasFixedSize(true)
         publicacionesRecyclerView.layoutManager = LinearLayoutManager(context)
-        publicacionesRecyclerView.adapter = PublicacionesAdapter(postsList,requireContext())
+        publicacionesRecyclerView.adapter = PublicacionesAdapter(postsList, requireContext())
 
     }
 
@@ -60,12 +58,10 @@ class ListaFragment : Fragment() {
         super.onStart()
         getPostFromFirebase()
 
-
-
-        btn_filtros.setOnClickListener {
-            lista.text =" a " +  postsList.get(0).id
-        }
     }
+
+
+
 
 
     fun getPostFromFirebase(){
@@ -97,6 +93,39 @@ class ListaFragment : Fragment() {
 
         }
     }
+
+    //todo ordenado por el parametro que se pase solo en forma desc
+    fun getPostFromFirebaseOrdered(orderBy: String){
+        val query = FirebaseFirestore.getInstance().collection("posts").orderBy(orderBy, Query.Direction.DESCENDING)
+
+        Log.d(ContentValues.TAG, "getin 1")
+        query.addSnapshotListener { snapshot, error ->
+            Log.d(ContentValues.TAG, "getin 2")
+            if (error != null) {
+                Log.d(ContentValues.TAG, "getin 3")
+                Log.d(ContentValues.TAG, "Listen failed.", error)
+                return@addSnapshotListener
+            }
+            postsList.clear()
+            if (snapshot != null) {
+
+                for (post in snapshot) {
+                    postsList.add(post.toObject())
+
+                    Log.d(ContentValues.TAG, "get in 4 " + postsList.toArray())
+                }
+                //todo
+                // Se tiene que llamar despues de que la lista post obtenga todos los post de firebase, sino rompe
+                configureRecyclerView()
+
+
+            } else {
+                Log.d(ContentValues.TAG, "Current data: null")
+            }
+
+        }
+    }
+
 
 
     // bottomsheet arreglar y cambiar cosas viejas
@@ -130,5 +159,23 @@ class ListaFragment : Fragment() {
 
     }
 
+    //todo relacionado a barra superior
+    //se llama para inflar el boton a la barra superior
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        // TODO Add your menu entries here
+        inflater.inflate(R.menu.historias_fragment_menu, menu)
+        super.onCreateOptionsMenu(menu!!, inflater!!)
+    }
+
+    //funcionalidad del boton de la barra superior //todo filtros
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            // Boton para mostrar el BottomSheet
+            R.id.menu_filter -> {
+                getPostFromFirebaseOrdered("precio")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
