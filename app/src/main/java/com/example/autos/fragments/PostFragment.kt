fc06e4f91e7dec891da5dd82aa3d3421eb9f7867
tomas <Homero_77>
2022-05-07 13:18:36 -0300

    material design

--- app/src/main/java/com/example/autos/fragments/PostFragment.kt
+++ app/src/main/java/com/example/autos/fragments/PostFragment.kt
@@ -1,13 +1,16 @@
 package com.example.autos.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import com.example.autos.R
+import com.google.android.material.slider.Slider
 
 
 class PostFragment : Fragment() {
@@ -21,13 +24,19 @@ class PostFragment : Fragment() {
         fragmentView = inflater.inflate(R.layout.fragment_post, container, false)
         id_post = fragmentView.findViewById(R.id.idPost)
 
+
+
         return fragmentView
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onStart() {
         super.onStart()
         val post = PostFragmentArgs.fromBundle(requireArguments()).selectedPost
-        id_post.text = post.id
+        id_post.text = "${post.id} ${post.año} ${post.marca} ${post.modelo} ${post.detalle}"
+
+
+
 
     }
 
